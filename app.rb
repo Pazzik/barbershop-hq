3f554eb8e04e5ab2c0c308ab250f6cb2fdec3c2d
Pazzik <user@user-NC210-NC110.(none)>
2015-09-08 21:28:06 +0300

    Update view  layout

--- app.rb
+++ app.rb
@@ -15,3 +15,49 @@ get '/' do
 	@barbers = Barber.order "created_at DESC"
 	erb :index	
 end
+
+before do 
+	@barbers = Barber.all
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username  	= params[:username]
+	@phone  	= params[:phone]
+	@date_time 	= params[:date_time]
+	@person		= params[:person]
+	@color		= params[:color_picker]
+
+	hh = { :username => 'Enter your name',
+		   :phone => 'Enter your phone',
+		   :date_time => 'Enter date and time'}
+
+	hh.each do |key,value|
+		if 	params[key] == ''
+			@error = hh[key]
+			return erb :visit
+		end
+	end
+
+	db = get_db
+	db.execute 'INSERT INTO
+	 			Users 
+	 			(
+	 				username, 
+	 				phone, 
+	 				datestamp, 
+	 				person, 
+	 				color
+	 			)
+				values(?,?,?,?,?)', [@username,@phone,@date_time,@person,@color]
+
+
+	f = File.open("./public/users.txt", "a") 
+	f.write("User: #{@username} Phone: #{@phone} Date and Time: #{@date_time} Person: #{@person} Color: #{@color}\n");
+	f.close;
+
+	erb "<h2>Thanks for visit</h2>"
+end
